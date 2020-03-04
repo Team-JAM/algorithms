0fd3ec1b2844ce712e8474317a20ccc55baf45e4
allisonkydy <allisnelly@gmail.com>
2020-03-04 14:37:00 -0600

    loop mining coins!

--- loop_coins.py
+++ loop_coins.py
@@ -3,11 +3,14 @@ Automatically navigates player to the well, decodes message using ls8 emulator, 
 """
 from gather_treasure import gather_treasure
 from utils import Queue
+from loop_ls8 import decode
+from loop_miner import mine
 import time
 import json
+import requests
 
-token = "a42506e85baef70dd9c66a7d0c090b10a3af26f8"
-base_url = "https://lambda-treasure-hunt.herokuapp.com/api/adv/"
+token = "b183cb414e3eae854e3930946d0c9370040ea416"
+base_url = "https://lambda-treasure-hunt.herokuapp.com/api/"
 headers = {"Authorization": f"Token {token}"}
 
 WELL = 55
@@ -74,10 +77,7 @@ def get_directions(starting_room, destination_room):
         room = path[-1][1]
         # If room is the desination, return the path
         if room == destination_room:
-            path_directions = get_pathing(path)
-            print(path_directions)
-            # travel(path_directions, token)
-            # return
+            return path
         # If it has not been visited...
         if room not in visited:
             # Mark it as visited
@@ -90,21 +90,80 @@ def get_directions(starting_room, destination_room):
             for next_room in adjacent_rooms:
                 queue.enqueue(path + [next_room])
 
-def navigate(room):
+def travel(path_directions, token):
+    # initiate travel mode
+    # time_for_next_action = time.time()
+
+    for instructions in path_directions:
+        travel_mode = instructions[0]
+        if travel_mode == 'fly' or travel_mode == 'move':
+            payload = {'direction': instructions[1], 'next_room_id': instructions[2]}
+        elif travel_mode == 'dash':
+            payload = {'direction': instructions[1], 'num_rooms': instructions[2], 'next_room_ids': instructions[3]}
+        elif travel_mode == 'recall':
+            payload = {}
+        
+        # sleep for cooldown
+        # time.sleep(max(time_for_next_action - time.time(), 0))
+        # move
+        r = requests.post(f'{base_url}adv/{travel_mode}/', headers=headers, json=payload)
+        print("moved to room: ", r.json()['room_id'])
+        print(r.json())
+        print()
+        # set cooldown
+        # time_for_next_action = time.time() + r.json()['cooldown']
+        time.sleep(r.json()['cooldown'])
+
+def navigate(starting_room, destination_room):
     start = time.time()
-    get_directions(0, 55)
+    path = get_directions(starting_room, destination_room)
+    path_directions = get_pathing(path)
+    travel(path_directions, token)
     end = time.time()
 
     print(f'Travel completed in {(end - start):.1f} seconds.')
 
-# while True:
+def well():
+    payload = {"name": "well"}
+    r = requests.post(base_url + "adv/examine/", headers=headers, json=payload)
+    print(r.json())
+    description = r.json()['description']
+    _, message = description.split('\n\n')
+
+    with open('wishing_well.ls8', 'w') as f:
+        f.write(message)
+    
+    time.sleep(r.json()['cooldown'])
+
+
+r = requests.get(base_url + "adv/init/", headers=headers)
+try:
+    current_room_id = r.json()['room_id']
+    print(f'current room: {current_room_id}')
+except KeyError:
+    print('Error: room_id does not exist')
+time.sleep(r.json()['cooldown'])
+
+while True:
     # navigate to well 
-navigate(WELL)
+    if current_room_id != WELL:
+        navigate(current_room_id, WELL)
 
     # examine well
+    well()
 
     # decode message
+    message = decode()
+    next_room = int(message[23:])
+    print(message + '\n')
 
     # navigate to room from message
+    navigate(WELL, next_room)
+    current_room_id = next_room
 
     # mine until receive a coin
+    result = {}
+    while result.get('messages') is None:
+        result = mine(headers)
+        time.sleep(result['cooldown'])
+    
